b05759308c292b605ad432f0ac65f111d5170fba
explt13 <141003949+explt13@users.noreply.github.com>
2024-03-08 18:02:20 +0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ import { js } from './gulp/tasks/js.js';
 import { images } from './gulp/tasks/images.js';
 import { videos } from './gulp/tasks/videos.js';
 import { otfToTtf, ttfToWoff, fontsStyle } from './gulp/tasks/fonts.js';
-import { svgSprites } from './gulp/tasks/svgsprites.js';
+import { svgSprites } from './gulp/tasks/svgSprites.js';
 import { zip } from './gulp/tasks/zip.js';
 import { ftp } from './gulp/tasks/ftp.js';
 
